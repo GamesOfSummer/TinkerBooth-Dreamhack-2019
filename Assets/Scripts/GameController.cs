39db8ac096a4e1a472075ed372efaed66d733ec5
Summer <summer.d.howard@gmail.com>
2019-05-25 16:06:42 -0500

    accept input

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+
+
+    public KnightScript knight;
+
+    private void Awake()
+    {
+
+
+      
+            if (instance)
+            {
+                Debug.Log("Destroying irrelevant GameController instance");
+                Destroy(this);
+            }
+
+            instance = this;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeKnightSpeed(InputField input)
+    {
+        Debug.Log("aaaaa -" + input.text);
+    }
+}
---
+++ Assets/Scripts/ValueChanged.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValueChanged : MonoBehaviour
+{
+
+
+    public void changeKnightSpeed(InputField input)
+    {
+        if (input.text.Length > 0)
+        {
+            Debug.Log("Text has been entered");
+        }
+        else if (input.text.Length == 0)
+        {
+            Debug.Log("Main Input Empty");
+        }
+
+        Debug.Log("!!! - " + input.text);
+    }
+}
